c3ec0a7136993de799666de233c89725e5e74d6e
Markku-Juhani O. Saarinen <mjos@iki.fi>
2019-10-11 10:55:52 +0100

    better parser

--- parselog.py
+++ parselog.py
@@ -8,20 +8,7 @@
 import sys
 import time
 
-core_clk = 96000000.0
-alg_name = ""
-sk_l = 0
-pk_l = 0
-ct_l = 0
-data_l = 0
 
-keygen 	= 0.0
-encaps 	= 0.0
-decaps 	= 0.0
-sign	= 0.0
-verify	= 0.0
-
-samp_l = 0
 
 # prettyprint units
 
@@ -47,89 +34,130 @@ def units(x):
 	return f'{x:8g}'
 
 def stats(cyc,pw):
-	return units(cyc)+"cycles" + "\t" + units(cyc/core_clk)+"s" + "\t" + \
-			units(pw)+"W" + "\t" + units(cyc*pw/core_clk)+"J"
+	return units(cyc)+"clk" + "\t" + units(cyc/cclk)+"s" + "\t" + \
+			units(pw)+"W" + "\t" + units(cyc*pw/cclk)+"J"
 
-# read everything from standard input and parse it
+# the main data dictionaries
 
-lns = sys.stdin.readlines()
-i = 0
-watt = []
-samp = []
-sampling = False
+kems = {}
+sgns = {}
 
-for lin in lns:
-	lv = lin.split();
-	ll = len(lv)
+# parse input files
 
-	#	basic data
-	if ll == 2 and lv[0] == "SystemCoreClock":
-		core_clk=float(lv[1])
-	if ll == 2 and lv[0] == "CRYPTO_ALGNAME":
-		alg_name=lv[1]
-	if ll == 2 and lv[0] == "CRYPTO_SECRETKEYBYTES":
-		sk_l = int(lv[1])
-	if ll == 2 and lv[0] == "CRYPTO_PUBLICKEYBYTES":
-		pk_l = int(lv[1])
-	if ll == 2 and lv[0] == "CRYPTO_CIPHERTEXTBYTES":
-		ct_l = int(lv[1])
-	if ll == 2 and lv[0] == "CRYPTO_BYTES":
-		data_l = int(lv[1])
+for fn in sys.argv[1:]:
+	
+	with open(fn, 'r') as f:
+		lns = f.readlines()
 
+	watt = []
+	samp = []
+	sampling = False
 
-	# process samples
-	if ll > 1 and lv[0] == "TimeStamp:":
-		samp = []
-		sampling = True
+	cclk = 96000000.0
+	algn = ""
+	sk_l = 0
+	pk_l = 0
+	ct_l = 0
+	ss_l = 0
 
-	if ll == 1 and lv[0] == "end":
-		sampling = False
+	keygen 	= 0.0
+	encaps 	= 0.0
+	decaps 	= 0.0
+	sign	= 0.0
+	verify	= 0.0
 
-	if ll == 1 and sampling:
-		samp.append(float(lv[0].replace("-","e-")))
+	i = 0
+	for lin in lns:
 
-	if ll >= 3 and lv[0] == "Acquisition" and lv[1] == "time:":
-		acq_time = float(lv[2])
-		watt.append(sum(samp)/acq_time)
+		#	line number
+		i = i + 1
+		lv = lin.split();
+		ll = len(lv)
 
-	if ll >= 4 and lv[0] == "Number" and lv[2] == "samples:":
-		if int(lv[3]) != len(samp):
-			print(f"{i:d}: mismatch. length={len(samp):d} ", lin)
+		#	basic parameters
+		if ll == 2 and lv[0] == "SystemCoreClock":
+			cclk = float(lv[1])
+		if ll == 2 and lv[0] == "CRYPTO_ALGNAME":
+			algn = lv[1]
+		if ll == 2 and lv[0] == "CRYPTO_SECRETKEYBYTES":
+			sk_l = int(lv[1])
+		if ll == 2 and lv[0] == "CRYPTO_PUBLICKEYBYTES":
+			pk_l = int(lv[1])
+		if ll == 2 and lv[0] == "CRYPTO_CIPHERTEXTBYTES":
+			ct_l = int(lv[1])
+		if ll == 2 and lv[0] == "CRYPTO_BYTES":
+			ss_l = int(lv[1])
 
-	if ll >= 3 and lv[0] == "***":
-#		print(lin)
-		if lv[1] == "KeyGen":
-			keygen=float(lv[2])
-		if lv[1] == "Encaps":
-			encaps=float(lv[2]) 
-		if lv[1] == "Decaps":
-			decaps=float(lv[2])
-		if lv[1] == "Sign":
-			sign=float(lv[2]) 
-		if lv[1] == "Verify":
-			verify=float(lv[2])
+		# process samples
+		if ll > 1 and lv[0] == "TimeStamp:":
+			samp = []
+			sampling = True
 
-	#	line number
-	i = i + 1
+		if ll == 1 and lv[0] == "end":
+			sampling = False
 
-kem = ct_l > 0;
+		if ll == 1 and sampling:
+			samp.append(float(lv[0].replace("-","e-")))
 
-if len(watt) < 4:
-	print("(Not enough data.)")
-	exit(1)
+		if ll >= 3 and lv[0] == "Acquisition" and lv[1] == "time:":
+			acq_time = float(lv[2])
+			watt.append(sum(samp)/acq_time)
+
+		if ll >= 4 and lv[0] == "Number" and lv[2] == "samples:":
+			if int(lv[3]) != len(samp):
+				print(f"{fn}:{i}[have {len(samp)}]{lin}", file=sys.stderr)
+
+		if ll >= 3 and lv[0] == "***":
+			if lv[1] == "KeyGen":
+				keygen = float(lv[2])
+			if lv[1] == "Encaps":
+				encaps = float(lv[2]) 
+			if lv[1] == "Decaps":
+				decaps = float(lv[2])
+			if lv[1] == "Sign":
+				sign = float(lv[2]) 
+			if lv[1] == "Verify":
+				verify = float(lv[2])
+
+	if len(watt) < 4:
+		print(f"{fn}: not enough data.", file=sys.stderr)
+		continue
+
+	kem = ct_l > 0
+
+	if fn.find("sneik") != -1:
+		algn = algn + "-sneik"		
+
+	if kem:
+		tot = keygen + encaps + decaps
+		if algn in kems and kems[algn][0][0] < tot:
+			continue
+		kems[algn] = ( (tot, keygen, encaps, decaps), tuple(watt), 
+						(sk_l, pk_l, ct_l, ss_l) )
+	else:
+		tot = keygen + sign + verify
+		if algn in sgns and sgns[algn][0][0] < tot:
+			continue
+		sgns[algn] = ( (tot, keygen, sign, verify), tuple(watt),
+						(sk_l, pk_l, ss_l) )
 
 #	summary results
 
-if kem:
-	print(f"{alg_name} (sk={sk_l}, pk={pk_l}, ct={ct_l}, ss={data_l})")
-	print("KeyGen:", stats(keygen,watt[1]))
-	print("Encaps:", stats(encaps,watt[2]))
-	print("Decaps:", stats(decaps,watt[3]))
-	print("TOTAL: ", stats(keygen+encaps+decaps,watt[0]))
-else:
-	print(f"{alg_name} (sk={sk_l}, pk={pk_l}, sl={data_l})")
-	print("KeyGen:", stats(keygen,watt[1]))
-	print("Sign:  ", stats(sign,watt[2]))
-	print("Verify:", stats(verify,watt[3]))
-	print("TOTAL: ", stats(keygen+sign+verify,watt[0]))
+for x in kems:
+	print(f"{x} (sk={kems[x][2][0]}, pk={kems[x][2][1]}, " +
+			f"ct={kems[x][2][2]}, ss={kems[x][2][3]})")
+	print("KeyGen:", stats(kems[x][0][1], kems[x][1][1]))
+	print("Encaps:", stats(kems[x][0][2], kems[x][1][2]))
+	print("Decaps:", stats(kems[x][0][3], kems[x][1][3]))
+	print("TOTAL: ", stats(kems[x][0][0], kems[x][1][0]))
+	print()
+
+for x in sgns:
+	print(f"{x} (sk={sgns[x][2][0]}, pk={sgns[x][2][1]}, " + 
+			f"sl={sgns[x][2][2]})")
+	print("KeyGen:", stats(sgns[x][0][1], sgns[x][1][1]))
+	print("Sign:  ", stats(sgns[x][0][2], sgns[x][1][2]))
+	print("Verify:", stats(sgns[x][0][3], sgns[x][1][3]))
+	print("TOTAL: ", stats(sgns[x][0][0], sgns[x][1][0]))
+	print()
 
